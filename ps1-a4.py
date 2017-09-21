e2de4abce549c6e0fcdaedb94156dda3871729a8
daramacmeanmain <dara.macmeanmain@outlook.ie>
2017-09-21 14:52:20 +0100

    FactorialDigitSumUpdated

--- ps1-a4.py
+++ ps1-a4.py
@@ -4,6 +4,7 @@ import math
 
 #calculation for 100! (line of code from https://stackoverflow.com/questions/3411657/calculating-factorial-in-python)
 fact = math.factorial(100)
+print ("100! is", fact)
 
 #output the sum of the factorial digits (line of code from https://stackoverflow.com/questions/14939953/sum-the-digits-of-a-number-python)
-print (sum(int(digit) for digit in str(fact)))
+print ("Sum of the digits of 100! is", sum(int(digit) for digit in str(fact)))
