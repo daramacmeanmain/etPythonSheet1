55ed2f4240e3b8565d5fef75beba17e9ad1974e6
daramacmeanmain <dara.macmeanmain@outlook.ie>
2017-09-21 13:42:49 +0100

    DateTimeComments

--- ps1-a2.py
+++ ps1-a2.py
@@ -1,4 +1,8 @@
+#Solution for Date and Time problem
+
 import time
 
+#Output time
 print ("The time is" + " " + time.strftime("%H:%M:%S"))
+#Output date
 print ("The date is" + " " + time.strftime("%x"))
