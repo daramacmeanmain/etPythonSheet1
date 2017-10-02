fa02c9f57bae96de575c17b022196a7694a53ec3
daramacmeanmain <dara.macmeanmain@outlook.ie>
2017-10-02 15:32:45 +0100

    Largest/Smallest

---
+++ ps1-a6.py
@@ -0,0 +1,7 @@
+print("Enter a series of numbers, separated by spaces")
+#user input for multiple integers (line of code from https://stackoverflow.com/questions/4663306/get-a-list-of-numbers-as-input-from-the-user)
+num = [int(x) for x in input().split()]
+
+#output the largest number in the user generated list followed by the smallest 
+print("The largest number is", max(num))
+print("The smallest number is", min(num))
