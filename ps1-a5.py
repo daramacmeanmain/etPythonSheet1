618fcf898fb2b9b3e4667f997af1b58507cbc5a2
daramacmeanmain <dara.macmeanmain@outlook.ie>
2017-10-06 20:51:14 +0100

    updated comments

--- ps1-a5.py
+++ ps1-a5.py
@@ -1,11 +1,14 @@
-#unfinished
+#Guessing Game
+#Unfinished - program doesn't stop
 
 import random
 
+#generate random number
 r = random.randrange(1,10)
 user = 0
 count = 0
 
+#determine whether the user input number is lesser than, greater than or equal to the random number
 def guess():
 	user = int(input("Enter number between 1 and 10:"), 16)
 	if user < r:
@@ -15,9 +18,11 @@ def guess():
 	else:
 		print("Correct, the number is", r)
 	return
-			
+
+#count the number of turns the user takes	
 while user != r:
 	count += 1
 	guess()
-	
+
+#output the number of turns	
 print ("Number of tries:", count)
